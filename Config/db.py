c6d2472291d4c27da615f3317586918e292874e9
Keiddy García <Keiddygarcia@gmail.com>
2023-11-07 06:20:46 -0500

    Update db.py

--- Config/db.py
+++ Config/db.py
@@ -5,7 +5,7 @@ from flask_marshmallow import Marshmallow
 app = Flask(__name__)
 
 #creamos las credenciales para conectarnos a la bd
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:15122001@localhost/kopay'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://KeiddyG:Kmgl1512@KeiddyG.mysql.pythonanywhere-services.com/KeiddyG$kopay'
 app.config['SQLALCHEMY_TRACK_MODIFACATIONS'] = True
 
 app.secret_key = "WebAvanzada"
