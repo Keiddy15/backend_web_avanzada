d99016309c34752658904dc3cb55a9ee415fb68e
Keiddy García <Keiddygarcia@gmail.com>
2023-11-10 11:08:06 -0500

    Update AuthRoutes.py

--- Routes/AuthRoutes.py
+++ Routes/AuthRoutes.py
@@ -11,12 +11,15 @@ auth = Blueprint("auth", __name__)
 def login():
     data = request.get_json()
     email = data['email']
-    password = data['password']
+    password = data['password'] 
     #Get the user
     user = Services.Users.get_user_by_email_and_password(email=email, password=password)
+    id_number = user.id_number
+    role = user.role
+    name = user.name
     #Validating user
     if user:
-        access_token = Utils.Token.generate_token(email, password)
+        access_token = Utils.Token.generate_token(email, id_number, role, name)
         return jsonify(access_token=access_token), 200
     else:
         return jsonify({'message': 'Incorrect credentials'}), 401
